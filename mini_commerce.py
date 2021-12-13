b862b00ded4e89a4dd09a28969b97b417218ef28
Ndcharles Nweke <ndcharles.edu@gmail.com>
2021-12-13 17:37:40 +0100

    ecommerce script

---
+++ mini_commerce.py
@@ -0,0 +1,53 @@
+# mini ecommerce store for turing-dev community
+'''
+This is very basic in the sense that:
+    - displays inventory onLoad
+    - receives input as an integer 
+    - stores your cart temporarily 
+    - and checks you at on request 
+'''
+
+print("Welcome to the Web Store")
+
+#inventory = print("Here is what we have in stock")
+inventory = {"Cup Cake": 500, "Coke": 200, "Bread": 300, "Milk": 250, "Chocolate": 150}
+print("Here is what we have in stock")
+for item in inventory.items():
+    print(item)
+
+buy_another = 1
+total_cost, total = 0, 0
+  
+while buy_another != 0:
+    order = int(input("Type the number of what you want to buy: "))
+    
+    if order == 1:
+        qty = int(input("How many Cup Cakes would you like?: "))
+        total = qty * 500
+        print(qty, f"Cup Cakes would cost you {total} naira")
+    
+    elif order == 2:
+        qty = int(input("How many bottles of Coke would you like?: "))
+        total = qty * 200
+        print(qty, f"bottles of Coke would cost you {total} naira")
+    
+    elif order == 3:
+        qty = int(input("How many loaves of bread would you like?: "))
+        total = qty * 300
+        print(qty, f"loaves of bread would cost you {total} naira")
+    
+    elif order == 4:
+        qty = int(input("How many tins of milk would you like?: "))
+        total = qty * 250
+        print(qty, f"tins of milk would cost you {total} naira")
+    
+    elif order == 5:
+        qty = int(input("How many packs of chocolate would you like?: "))
+        total = qty * 150
+        print(qty, f"packs of chocolate would cost you {total} naira")        
+    
+    total_cost += total
+    
+    buy_another = int(input("Would you like to shop for anything else?\nEnter 1 to continue or 0 to checkout: "))
+
+print(f"Your shopping cart costs {total_cost}\nThank you for shopping with us")    
